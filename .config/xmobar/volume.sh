9a5484728dda38ce338de27c0e013ff99b08e6b6
Filip Peterek <fpeterek@seznam.cz>
2026-05-27 02:51:05 +0200

    Volume

--- .config/xmobar/volume.sh
+++ .config/xmobar/volume.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
-VOLUME=`pamixer --get-volume`
+VOLUME=`pamixer --get-volume-human`
 
-echo "<box type=Bottom width=2 color=#f7b374><fc=#f7b374><hspace=5/><fn=1>󰖀</fn><hspace=5/>$VOLUME%<hspace=5/></fc></box>"
+# Icons
+# 
+# 
+# 󰖀
+# 
+# 
+# 
+
+ICON=""
+
+if [ $VOLUME == 'muted' ]
+then
+    VOLUME="M"
+    ICON=""
+fi
+
+echo "<box type=Bottom width=2 color=#f7b374><fc=#f7b374><hspace=5/><fn=1>$ICON</fn><hspace=5/>$VOLUME<hspace=5/></fc></box>"
